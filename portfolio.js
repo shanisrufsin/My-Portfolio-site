e454de527133d4a760f8807241816e5d930d00f2
shanisrufsin <150383489+shanisrufsin@users.noreply.github.com>
2024-02-16 00:47:50 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed= new Typed('#text', {
+    strings:["a FrontEnd Developer" , "a Web Developer" , "an Entrepreneur"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
